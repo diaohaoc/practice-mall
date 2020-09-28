cd5775385ed3e353eeb454477c4ba77e3227bb67
Diao-hao <522339634@qq.com>
2020-09-28 23:33:25 +0800

    配置各种依赖

--- mall-commons/src/main/java/com/mymall/commons/log/WebLogAspect.java
+++ mall-commons/src/main/java/com/mymall/commons/log/WebLogAspect.java
@@ -1,5 +1,7 @@
-package com.mymall.commons.log;/**
-* Create by DiaoHao on 2020/9/28 16:50
-*/
+package com.mymall.commons.log;
+
+/**
+ * Create by DiaoHao on 2020/9/28 16:50
+ */
 public class WebLogAspect {
 }
